8940c2f9ff290efe5ac0d61e75d38cdb1e8b9ac6
Samrat <mitra.stark.68@gmail.com>
2023-02-09 02:48:01 +0530

    details.html added

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('items/', include('item.urls')),
     path('',index,name='index'),
     path('contact/',contact,name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
